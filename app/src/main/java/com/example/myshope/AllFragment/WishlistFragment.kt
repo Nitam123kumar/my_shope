e7c61ea1481512a7fa40c5a63fa9ff6cadef2917
Nitam123kumar <155878704+Nitam123kumar@users.noreply.github.com>
2024-12-07 15:57:41 +0530

    profile update and searching

--- app/src/main/java/com/example/myshope/AllFragment/WishlistFragment.kt
+++ app/src/main/java/com/example/myshope/AllFragment/WishlistFragment.kt
@@ -2,10 +2,11 @@ package com.example.myshope.AllFragment
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myshope.AllAdapter.WomenProductsAdapter
@@ -17,10 +18,12 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
 class WishlistFragment : Fragment() {
-        lateinit var allDataRecyclerView: RecyclerView
+    lateinit var allDataRecyclerView: RecyclerView
     lateinit var womenProductsAdapter: WomenProductsAdapter
     var womenProductsList = ArrayList<womenProductsDataModel>()
     lateinit var db: FirebaseDatabase
+    lateinit var searchView2: SearchView
+    var filterList = ArrayList<womenProductsDataModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,20 +36,46 @@ class WishlistFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view=inflater.inflate(R.layout.fragment_wishlist, container, false)
+        val view = inflater.inflate(R.layout.fragment_wishlist, container, false)
 
-        allDataRecyclerView=view.findViewById(R.id.all_Data_RecyclerView)
+        allDataRecyclerView = view.findViewById(R.id.all_Data_RecyclerView)
 
-        allDataRecyclerView.layoutManager=GridLayoutManager(requireContext(),2,GridLayoutManager.VERTICAL,false)
-        womenProductsAdapter= WomenProductsAdapter(womenProductsList)
-        allDataRecyclerView.adapter=womenProductsAdapter
-        db= FirebaseDatabase.getInstance()
+        allDataRecyclerView.layoutManager =
+            GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+        womenProductsAdapter = WomenProductsAdapter(womenProductsList)
+        allDataRecyclerView.adapter = womenProductsAdapter
+        db = FirebaseDatabase.getInstance()
         womenProductsGetData()
+
+        searchView2 = view.findViewById(R.id.searchView2)
+        searchView2.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newtext: String?): Boolean {
+                filterList.clear()
+
+                if (newtext != null) {
+                    womenProductsList.forEach {
+                        if (it.womenTittle?.lowercase()?.contains(newtext.toLowerCase()) == true) {
+                            filterList.add(it)
+                        }
+
+                    }
+                }
+
+                womenProductsAdapter.filteredList(filterList)
+                womenProductsAdapter.notifyDataSetChanged()
+                return true
+            }
+
+        })
 
         return view
     }
 
-   private fun womenProductsGetData() {
+    private fun womenProductsGetData() {
         db.getReference("users").child("womenProducts").addValueEventListener(object :
             ValueEventListener {
             @SuppressLint("NotifyDataSetChanged")
